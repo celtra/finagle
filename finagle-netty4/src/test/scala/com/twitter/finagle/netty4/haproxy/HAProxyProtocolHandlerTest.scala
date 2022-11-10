b9635743bf58f8f17118a152a3bc8bf60ee46ddd
Miha Rugelj <rugelj.miha@gmail.com>
2022-11-10 21:01:15 +0100

    HA proxy handler test

---
+++ finagle-netty4/src/test/scala/com/twitter/finagle/netty4/haproxy/HAProxyProtocolHandlerTest.scala
@@ -0,0 +1,64 @@
+package com.twitter.finagle.netty4.haproxy
+
+import io.netty.buffer.Unpooled
+import io.netty.channel.embedded.EmbeddedChannel
+import io.netty.handler.codec.haproxy.{
+  HAProxyCommand,
+  HAProxyMessage,
+  HAProxyProtocolVersion,
+  HAProxyProxiedProtocol
+}
+import io.netty.util.CharsetUtil
+import org.scalatest.funsuite.AnyFunSuite
+
+class HAProxyProtocolHandlerTest extends AnyFunSuite {
+
+  test("Saves HA proxy protocol properties to channel attributes") {
+    val channel = new EmbeddedChannel(new HAProxyProtocolHandler())
+    val msg = defaultHAProxyMessage()
+    channel.writeInbound(msg)
+    channel.flushInbound()
+
+    // Message should not be propagated down the channel
+    assert(channel.inboundMessages().size() == 0)
+
+    assert(channel.attr(HAProxyProtocolHandler.SourceAddressAttribute).get().getHostAddress == "1.1.1.1")
+    assert(channel.attr(HAProxyProtocolHandler.SourcePortAttribute).get() == 1024)
+    assert(channel.attr(HAProxyProtocolHandler.DestinationAddressAttribute).get().getHostAddress == "2.2.2.2")
+    assert(channel.attr(HAProxyProtocolHandler.DestinationPortAttribute).get() == 8080)
+  }
+
+  test("Propagates message down the channel if not HA proxy message") {
+    val channel = new EmbeddedChannel(new HAProxyProtocolHandler())
+    val msg = Unpooled.buffer().writeBytes("hello".getBytes(CharsetUtil.UTF_8))
+
+    assert(channel.writeInbound(msg))
+    assert(channel.inboundMessages().size() == 1)
+
+    Seq(
+      HAProxyProtocolHandler.SourceAddressAttribute,
+      HAProxyProtocolHandler.SourcePortAttribute,
+      HAProxyProtocolHandler.DestinationAddressAttribute,
+      HAProxyProtocolHandler.DestinationPortAttribute
+    ).foreach { attr =>
+      assert(channel.attr(attr).get() == null)
+    }
+  }
+
+  private def defaultHAProxyMessage(
+    sourceAddr: String = "1.1.1.1",
+    destAddr: String = "2.2.2.2",
+    sourcePort: Int = 1024,
+    destPort: Int = 8080
+  ): HAProxyMessage = {
+    new HAProxyMessage(
+      HAProxyProtocolVersion.V1,
+      HAProxyCommand.PROXY,
+      HAProxyProxiedProtocol.TCP4,
+      sourceAddr,
+      destAddr,
+      sourcePort,
+      destPort
+    )
+  }
+}
